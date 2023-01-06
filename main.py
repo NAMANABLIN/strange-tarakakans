9fc822f7fbbfb712701d2b68b02a1e41bd8498b9
qwertdk <karinadaianova45@gmail.com>
2023-01-06 20:06:39 +0500

    add menu

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
 from classes import *
 from config import *
+from random import randint
 from menu_1 import main_menu
+
+def generate_level(level):
+    player, x, y = None, None, None
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == '.':
+                Tile('empty', x, y)
+            elif level[y][x] == '#':
+                Tile('wall', x, y)
+    return x, y
+
+
+def load_level(filename):
+    filename = os.path.join(filename)
+    if not os.path.isfile(filename):
+        print(f"Файл с уровнем '{filename}' не найден.\n Возможно этого уровня нет в папке 'levels'")
+        sys.exit()
+    else:
+        with open(filename, 'r') as mapFile:
+            level_map = [line.strip() for line in mapFile]
+
+    max_width = max(map(len, level_map))
+
+    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
+
+
 pg.init()
 main_menu()
 
@@ -13,30 +40,20 @@ FPS = 60
 
 speed = 15
 
+enemies = []
+for x in range(10):
+    if randint(0,1):
+        enemies.append(TarakanEnemy(randint(0, 350), randint(0, 250)))
+    else:
+        enemies.append(TarakanEnemy(randint(450, 800), randint(350, 600)))
+
+player_bullets = []
 camera = Camera(size)
 
-player, level_x, level_y = generate_level(load_level('kek.txt'))
-
+level_x, level_y = generate_level(load_level('kek.txt'))
+player = Player(400,300)
 while True:
-    sc.fill(BLACK)
-
-    if player.alive():
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                pg.quit()
-                sys.exit()
-        tiles_group.draw(sc)
-        wall_group.draw(sc)
-        bullets_group.draw(sc)
-        enemys_group.draw(sc)
-        lol = pg.Surface(size)
-        lol.fill((0, 0, 0))
-        lol.set_alpha(100)
-        sc.blit(lol, (0, 0))
-        pg.display.update()
-        clock.tick(FPS)
-
-        continue
+    sc.fill(MALAHIT)
     for event in pg.event.get():
         if event.type == pg.QUIT:
             pg.quit()
@@ -44,9 +61,10 @@ while True:
         elif event.type == pg.MOUSEBUTTONDOWN:
             if event.button == 1:
                 mouse_x, mouse_y = event.pos
-                PlayerBullet(player.rect.x + 40, player.rect.y - 30, mouse_x, mouse_y)
+                player_bullets.append(PlayerBullet(player.x, player.y, mouse_x, mouse_y))
 
     keys = pg.key.get_pressed()
+
 
     if keys[pg.K_LEFT] or keys[pg.K_a]:
         player.move('лево')
@@ -65,6 +83,8 @@ while True:
     enemys_group.draw(sc)
     player_group.draw(sc)
 
+
+
     camera.update(player)
     for sprite in all_sprites:
         camera.apply(sprite)
@@ -76,4 +96,6 @@ while True:
         lol += 35
 
     pg.display.update()
+
+
     clock.tick(FPS)
--- menu_1.py
+++ menu_1.py
@@ -4,6 +4,12 @@ from config import *
 
 size = width, height = 800, 600
 
+forward = 'W'
+back = 'S'
+left = 'A'
+right = 'D'
+shooting = 'ЛКМ'
+
 
 def menu(screen):
     pygame.display.set_caption('menu')
@@ -41,15 +47,20 @@ def continuegame():
     # screen_value = 'continue'
 
 
-def control():
+def control(forward, back, left, right, shooting):
     screen = pygame.display.set_mode(size)
     pygame.display.set_caption('control')
     screen.fill((60, 113, 125))
-    screen.blit(font.render("Вперёд          W", 1, font_color), (width // 30 + 10, height - 550, 180, 50))
-    screen.blit(font.render("Назад             S", 1, font_color), (width // 30 + 10, height - 515, 180, 50))
-    screen.blit(font.render("Влево             A", 1, font_color), (width // 30 + 10, height - 485, 180, 50))
-    screen.blit(font.render("Вправо           D", 1, font_color), (width // 30 + 10, height - 450, 180, 50))
-    screen.blit(font.render("Стрельба        ЛКМ", 1, font_color), (width // 30 + 10, height - 415, 180, 50))
+    screen.blit(font.render("Вперёд" + (18 - 6 - len(forward)) * ' ' + forward, 1, font_color),
+                (width // 30 + 10, height - 550, 180, 50))
+    screen.blit(font.render("Назад" + (19 - 5 - len(back)) * ' ' + back, 1, font_color),
+                (width // 30 + 10, height - 515, 180, 50))
+    screen.blit(font.render("Влево" + (19 - 5 - len(left)) * ' ' + left, 1, font_color),
+                (width // 30 + 10, height - 485, 180, 50))
+    screen.blit(font.render("Вправо" + (18 - 6 - len(right)) * ' ' + right, 1, font_color),
+                (width // 30 + 10, height - 450, 180, 50))
+    screen.blit(font.render("Стрельба" + (18 - 8 - len(shooting)) * ' ' + shooting, 1, font_color),
+                (width // 30 + 10, height - 415, 180, 50))
     buttons_coord = [(width // 2 - 190 // 2, height - 100, 190, 50)]
     pygame.draw.rect(screen, (50, 92, 102), buttons_coord[0], 0)
     text = font.render("Назад", 1, font_color)
@@ -58,7 +69,7 @@ def control():
     return buttons_coord, screen_value
 
 
-def options():
+def options(s, m):
     pygame.display.set_caption('options')
     screen = pygame.display.set_mode(size)
     screen.fill((60, 113, 125))
@@ -84,8 +95,8 @@ def options():
     pygame.draw.line(screen, (58, 65, 66), (width - 550, height - 455), (width - 300, height - 455), width=5)
     pygame.draw.line(screen, (58, 65, 66), (width - 550, height - 395), (width - 300, height - 395), width=5)
 
-    sound = pygame.draw.rect(screen, (50, 92, 102), (width - 550, height - 470, 10, 30))
-    music = pygame.draw.rect(screen, (50, 92, 102), (width - 550, height - 410, 10, 30))
+    sound = pygame.draw.rect(screen, (50, 92, 102), (s, height - 470, 10, 30))
+    music = pygame.draw.rect(screen, (50, 92, 102), (m, height - 410, 10, 30))
 
     sounds = [(sound.x, sound.y, 10, 30), (music.x, music.y, 10, 30)]
 
@@ -102,27 +113,32 @@ def button(buttons_coord, screen):
 
 
 def main_menu():
+    global forward, back, left, right, shooting
+    key = ''  # !!!!!
+    control_list = [forward, back, left, right, shooting]
     pygame.init()
     screen = pygame.display.set_mode(size)
 
     buttons, screen_value = menu(screen)
     running = True
     clock = pygame.time.Clock()
+    s = m = width - 550
+    buttons, screen_value = menu(screen)
+    running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
+                running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
-                x, y = event.pos
+                x = event.pos[0]
+                y = event.pos[1]
                 if screen_value == 'menu':
                     for i in buttons:
                         if i[0] <= x <= i[0] + i[2] and i[1] <= y <= i[1] + i[3]:
                             if buttons.index(i) == 0:  # exit
                                 running = False
                             elif buttons.index(i) == 1:  # options
-                                buttons, screen_value, sounds, sound, music = options()
-                                print(sounds)
+                                buttons, screen_value, sounds, sound, music = options(s, m)
                             elif buttons.index(i) == 2:  # new game
                                 screen_value = newgame()
                             elif buttons.index(i) == 3:  # continue
@@ -131,24 +147,66 @@ def main_menu():
                 if screen_value == 'options':
                     if sounds[0][0] <= x <= sounds[0][0] + sounds[0][2] and sounds[0][1] <= y <= sounds[0][1] + \
                             sounds[0][3]:  # sound
-                        if pygame.mouse.get_pressed()[0]:
-                            pass
+                        while pygame.mouse.get_pressed()[0]:
+                            if width - 550 <= pygame.mouse.get_pos()[0]:
+                                if pygame.mouse.get_pos()[0] <= width - 305:
+                                    s = pygame.mouse.get_pos()[0]
+                                else:
+                                    s = width - 305
+                            else:
+                                s = width - 550
+                            buttons, screen_value, sounds, sound, music = options(s, m)
                     if sounds[1][0] <= x <= sounds[1][0] + sounds[1][2] and sounds[1][1] <= y <= sounds[1][1] + \
                             sounds[1][3]:  # music
-                        if pygame.mouse.get_pressed()[0]:
-                            pass
+                        while pygame.mouse.get_pressed()[0]:
+                            if width - 550 <= pygame.mouse.get_pos()[0]:
+                                if pygame.mouse.get_pos()[0] <= width - 305:
+                                    m = pygame.mouse.get_pos()[0]
+                                else:
+                                    m = width - 305
+                            else:
+                                m = width - 550
+                            buttons, screen_value, sounds, sound, music = options(s, m)
                     for i in buttons:
                         if i[0] <= x <= i[0] + i[2] and i[1] <= y <= i[1] + i[3]:
                             if buttons.index(i) == 3:  # back
                                 buttons, screen_value = menu(screen)
                             elif buttons.index(i) == 2:  # control
-                                buttons, screen_value = control()
+                                buttons, screen_value = control(forward, back, left, right, shooting)
                                 break
-                if screen_value == 'control':
+                elif screen_value == 'control':
                     if buttons[0][0] <= x <= buttons[0][0] + buttons[0][2] and buttons[0][1] <= y <= buttons[0][1] + \
                             buttons[0][3]:
-                        buttons, screen_value, sounds, sound, music = options()
-                if screen_value == 'newgame':
-                    running = False
+                        buttons, screen_value, sounds, sound, music = options(s, m)
+                    else:
+                        if event.button == 1 and 'ЛКМ' in control_list and key == '':
+                            key = control_list.index('ЛКМ')
+                            control_list[control_list.index('ЛКМ')] = ''
+                        elif event.button == 3 and 'ПКМ' in control_list and key == '':
+                            key = control_list.index('ПКМ')
+                            control_list[control_list.index('ПКМ')] = ''
+                        elif event.button == 1 and key != '' and 'ЛКМ' not in control_list:
+                            control_list[key] = 'ЛКМ'
+                            key = ''
+                        elif event.button == 3 and key != '' and 'ПКМ' not in control_list:
+                            control_list[key] = 'ПКМ'
+                            key = ''
+                        forward, back, left, right, shooting = control_list
+                        control(forward, back, left, right, shooting)
+                        control_list = [forward, back, left, right, shooting]
+            if event.type == pygame.KEYDOWN:
+                if event.key in range(97, 122):
+                    if screen_value == 'control':
+                        if chr(event.key).upper() in control_list and '' not in control_list:
+                            key = control_list.index(chr(event.key).upper())
+                            control_list[control_list.index(chr(event.key).upper())] = ''
+                            forward, back, left, right, shooting = control_list
+                            control(forward, back, left, right, shooting)
+                        elif chr(event.key).upper() not in control_list and key != '':
+                            control_list[key] = chr(event.key).upper()
+                            forward, back, left, right, shooting = control_list
+                            control(forward, back, left, right, shooting)
+                            key = ''
+                        control_list = [forward, back, left, right, shooting]
         pygame.display.flip()
-        clock.tick(60)
+    pygame.quit()
