4f33d2d8dc34ac57b6795f10a7e33bf507edf55e
NAMANABLIN <N1m1n1Bl1n@yandex.ru>
2023-01-19 08:02:28 +0500

    final commit

---
+++ resources/levels/lab.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="lab" tilewidth="32" tileheight="32" tilecount="15" columns="3">
+ <image source="../images/png/tilemap.png" width="96" height="160"/>
+</tileset>
